7bcad491691d61e35c7491018cb76a93f3239eac
aviwembekeni <aviwe3226@gmail.com>
2021-01-21 15:18:36 +0200

    added more tests

--- src/functions/standardizeDifferentScales.test.ts
+++ src/functions/standardizeDifferentScales.test.ts
@@ -1,7 +1,15 @@
 import standardizeDifferentScales from './standardizeDifferentScales';
 
 describe('Testing standardizing different scales function function', (): void => {
-  it('should bring back standardized value from a scale of 0 to 10', (): void => {
+  it('should bring back standardized value (10) from a scale of 0 to 10', (): void => {
     expect(standardizeDifferentScales(4, 0, 4, 10)).toEqual(10);
+  });
+
+  it('should bring back standardized value (0) from a scale of 0 to 10', (): void => {
+    expect(standardizeDifferentScales(0, 0, 4, 10)).toEqual(0);
+  });
+
+  it('should bring back standardized value (5) from a scale of 0 to 10', (): void => {
+    expect(standardizeDifferentScales(2, 0, 4, 10)).toEqual(5);
   });
 });
