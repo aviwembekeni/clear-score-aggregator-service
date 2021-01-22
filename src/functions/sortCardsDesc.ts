3b9911ed19249010c63006d47963718a980c710c
aviwembekeni <aviwe3226@gmail.com>
2021-01-22 15:21:44 +0200

    sorting in descending order instead of ascending

--- src/functions/sortCardsDesc.ts
+++ src/functions/sortCardsDesc.ts
@@ -1,5 +1,5 @@
 import RecommendedCreditCardsResponse from '../models/RecommendedCreditCardsResponse.model';
 
 export default (cards: RecommendedCreditCardsResponse[]): RecommendedCreditCardsResponse[] => {
-  return cards.sort((a, b) => a.cardScore - b.cardScore);
+  return cards.sort((a, b) => b.cardScore - a.cardScore);
 };
